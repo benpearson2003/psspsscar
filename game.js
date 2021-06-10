c0c85bca4a5118554afcd68998e8cb1312ad5c58
Benjamin Pearson <bpearson@arklegaudit.gov>
2021-06-10 16:45:49 -0500

    first commit

---
+++ game.js
@@ -0,0 +1,7 @@
+function piss() {
+    let offsetHeight = document.getElementById("piss").offsetHeight;
+    document.getElementById("piss").style.height = offsetHeight + 1 + "%";
+}
+function dump() {
+    document.getElementById("piss").style.height = 0;
+}
